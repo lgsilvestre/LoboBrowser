4b199deaceb9d2615e6afd8f9b8ede5f138524b8
HP <HP@192.168.2.4>
2014-03-18 22:30:29 -0400

    Commit Main

---
+++ Lobo/src/MainLobo.java
@@ -0,0 +1,13 @@
+
+public class MainLobo {
+
+	public MainLobo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
